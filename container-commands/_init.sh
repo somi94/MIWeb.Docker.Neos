f77861db4b7222399ae3661bf57899309dd161e1
Sönke Michelsen <39090205+somi94@users.noreply.github.com>
2019-02-13 20:02:09 +0100

    Update _init.sh

--- container-commands/_init.sh
+++ container-commands/_init.sh
@@ -2,34 +2,34 @@
 
 # build variables
 if [[ -z "$BUILD_PATH_RELEASE" ]]; then
-  export $BUILD_PATH_RELEASE=/var/www/html
+  export BUILD_PATH_RELEASE=/var/www/html
 fi
 if [[ -z "$BUILD_PATH_DIST" ]]; then
-  export $BUILD_PATH_DIST=/usr/share/neos-project
+  export BUILD_PATH_DIST=/usr/share/neos-project
 fi
 if [[ -z "$BUILD_PATH_BASE" ]]; then
-  export $BUILD_PATH_BASE=/usr/share/neos-base
+  export BUILD_PATH_BASE=/usr/share/neos-base
 fi
 if [[ -z "$BUILD_PATH_UTILS" ]]; then
-  export $BUILD_PATH_UTILS=/usr/share/neos-utils
+  export BUILD_PATH_UTILS=/usr/share/neos-utils
 fi
 if [[ -z "$BUILD_REPOSITORY" ]]; then
-    export $BUILD_REPOSITORY="https://github.com/neos/neos-base-distribution.git"
+    export BUILD_REPOSITORY="https://github.com/neos/neos-base-distribution.git"
 fi
 
 # flow variables
 if [[ -z "$FLOW_CONTEXT" ]]; then
-    export $FLOW_CONTEXT=Development
+    export FLOW_CONTEXT=Development
 fi
 if [[ -z "$NEOS_USER_NAME" ]]; then
-    export $NEOS_USER_NAME=root
+    export NEOS_USER_NAME=root
 fi
 if [[ -z "$NEOS_USER_PASSWORD" ]]; then
-    export $NEOS_USER_PASSWORD=root
+    export NEOS_USER_PASSWORD=root
 fi
 if [[ -z "$NEOS_USER_FIRSTNAME" ]]; then
-    export $NEOS_USER_FIRSTNAME="Firstname"
+    export NEOS_USER_FIRSTNAME="Firstname"
 fi
 if [[ -z "$NEOS_USER_LASTNAME" ]]; then
-    export $NEOS_USER_LASTNAME="Lastname"
+    export NEOS_USER_LASTNAME="Lastname"
 fi
