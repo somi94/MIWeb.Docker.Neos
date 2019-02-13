8a72ec9bef5f73f6a4c652dde4fc044f326b8c11
Sönke Michelsen <39090205+somi94@users.noreply.github.com>
2019-02-13 15:12:20 +0100

    Create config.sh

---
+++ container-commands/config.sh
@@ -0,0 +1,33 @@
+# build variables
+if [[ -z "$BUILD_PATH_RELEASE" ]]; then
+  export $BUILD_PATH_RELEASE=/var/www/html
+fi
+if [[ -z "$BUILD_PATH_DEV" ]]; then
+  export $BUILD_PATH_DEV=/usr/share/neos
+fi
+if [[ -z "$BUILD_PATH_BASE" ]]; then
+  export $BUILD_PATH_BASE=/usr/share/neos-base
+fi
+if [[ -z "$BUILD_PATH_UTILS" ]]; then
+  export $BUILD_PATH_UTILS=/usr/share/neos-utils
+fi
+if [[ -z "$BUILD_REPOSITORY" ]]; then
+    export $BUILD_REPOSITORY="https://github.com/neos/neos-base-distribution.git"
+fi
+
+# flow variables
+if [[ -z "$FLOW_CONTEXT" ]]; then
+    export $FLOW_CONTEXT=Development
+fi
+if [[ -z "$FLOW_USER" ]]; then
+    export $FLOW_USER=root
+fi
+if [[ -z "$FLOW_PASSWORD" ]]; then
+    export $FLOW_PASSWORD=root
+fi
+if [[ -z "$FLOW_FIRSTNAME" ]]; then
+    export $FLOW_FIRSTNAME="Firstname"
+fi
+if [[ -z "$FLOW_FIRSTNAME" ]]; then
+    export $FLOW_FIRSTNAME="Lastname"
+fi
