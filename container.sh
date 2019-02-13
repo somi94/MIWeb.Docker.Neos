d0b51fce75c534e17191a11e08fa8eaa4e9f4b56
Sönke Michelsen <39090205+somi94@users.noreply.github.com>
2019-02-13 18:38:53 +0100

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-command=$1
 command_dir="/usr/share/neos-utils"
+if[[ -n "$BUILD_PATH_UTILS" ]]; then
+    command_dir=$BUILD_PATH_UTILS
+fi
+
+command=$1
 
 if [[ ! -f "$command_dir/$command.sh" ]]; then
     echo "Invalid command '$1'."
