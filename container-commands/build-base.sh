29bf6a863cab7bab80b2b921b57b9ed363eb73d1
Sönke Michelsen <39090205+somi94@users.noreply.github.com>
2019-02-14 23:23:33 +0100

    Update build-base.sh

--- container-commands/build-base.sh
+++ container-commands/build-base.sh
@@ -59,6 +59,7 @@ echo "################################"
 echo "# Releasing base files"
 echo "################################"
 echo ""
+rm -rf $BUILD_PATH_RELEASE/*
 cp -r $BUILD_PATH_BASE/* $BUILD_PATH_RELEASE
 if [ $? -ne 0 ]; then
     echo "Base file release failed. Aborting..."
