fa685453e5e4124c44d3f21ce9d3502d4ed0bfe8
somi <soenke.mi@hotmail.de>
2019-02-19 01:05:17 +0100

    Removed data permissions from app setup

--- container-commands/setup/app.sh
+++ container-commands/setup/app.sh
@@ -26,9 +26,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Updating data ownership (user: '$SYSTEM_USER_NAME')..."
-chown -R "$SYSTEM_USER_NAME":www-data $BUILD_PATH_BASE/Data
-if [ $? -ne 0 ]; then
-    echo "Updating data ownership failed. Aborting..."
-    exit 1
-fi
+#echo "Updating data ownership (user: '$SYSTEM_USER_NAME')..."
+#chown -R "$SYSTEM_USER_NAME":www-data $BUILD_PATH_BASE/Data
+#if [ $? -ne 0 ]; then
+#    echo "Updating data ownership failed. Aborting..."
+#    exit 1
+#fi
